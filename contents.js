7a2eb89e8623ff5a4d4921769d8e826e1bded0a5
vgonzalez165 <vgonzalez165@gmail.com>
2023-01-11 08:09:03 +0100

    añadidas ut05 y ut06 a índice

--- contents.js
+++ contents.js
@@ -110,5 +110,93 @@ export let uts = [
                 file: "05_compartidas.md"
             }
         ],
+    },
+    {
+        number: "UT05",
+        title: "Windows 10. Administración",
+        hours: 22,
+        folder: "ut05",
+        contents: [
+            {
+                title: "Gestión de procesos",
+                file: "01_procesos.md",
+            },
+            {
+                title: "El administrador de tareas",
+                file: "02_taskmanager.md",
+            },
+            {
+                title: "Servicios",
+                file: "03_servicios.md"
+            },
+            {
+                title: "El monitor de recursos",
+                file: "04_monitor_recursos.md",
+            },
+            {
+                title: "El visor de eventos",
+                file: "05_visor_eventos.md"
+            },
+            {
+                title: "Administración de energía",
+                file: "06_energia.md"
+            },
+            {
+                title: "Directivas de grupo local",
+                file: "07_directivas.md",
+            },
+            {
+                title: "El programador de tareas",
+                file: "08_programador.md"
+            }
+        ],
+    },
+    {
+        number: "UT06",
+        title: "Powershell",
+        hours: 22,
+        folder: "ut06",
+        contents: [
+            {
+                title: "Introducción a Powershell",
+                file: "01_introduccion.md",
+            },
+            {
+                title: "Control de salida con pipelines",
+                file: "02_pipelines.md",
+            },
+            {
+                title: "Tipos de datos y variables",
+                file: "03_tipos_datos_y_variables.md"
+            },
+            {
+                title: "Usuarios y grupos desde Powershell",
+                file: "04_usuarios.md",
+            },
+            {
+                title: "Funciones avanzadas en la gestión de usuarios",
+                file: "05_usuarios_avanzado.md"
+            },
+            {
+                title: "Conexiones remotas",
+                file: "06_conexion_remota.md"
+            },
+            {
+                title: "Controlando Hyper-V con Powershell",
+                file: "07_hyperv.md",
+            },
+            {
+                title: "Powershell y el almacenamiento",
+                file: "08_almacenamiento.md"
+            },
+            {
+                title: "Uso del sistema de ficheros desde Powershell",
+                file: "09_sistema_ficheros.md",
+            },
+            {
+                title: "Miscelánea",
+                file: "10_miscelanea.md"
+            }
+        ],
     }
 ]
